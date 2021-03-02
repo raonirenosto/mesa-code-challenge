8525df8b37e19d63728d8efe0cdbea49d21af6df
Raoni Coelho <raonicoelho@gmail.com>
2021-03-02 13:51:02 -0300

    Add feature for updating user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :authenticate, only: [:show]
+  before_action :authenticate, only: [:show, :update]
 
   def show
     user = User.find(params[:id])
-    render json: user.as_json(only: [:id, :email]), status: :ok
+    render json: user.as_json(only: fields_only), status: :ok
   end
 
   def create
@@ -12,6 +12,19 @@ class Api::V1::UsersController < ApplicationController
     user.password = params[:password]
     user.save!
 
-    render json: user.as_json(only: [:id, :email]), status: :created
+    render json: user.as_json(only: fields_only), status: :created
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.email = params[:email]
+    user.password = params[:password]
+    user.save!
+
+    render json: user.as_json(only: fields_only), status: :ok
+  end
+
+  def fields_only
+    [:id, :email]
   end
 end
