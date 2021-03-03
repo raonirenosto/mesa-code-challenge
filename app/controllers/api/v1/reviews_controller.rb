24267df6c13f06841766b1de67736bbc4b4260cf
Raoni Renosto Coelho <raonicoelho@gmail.com>
2021-03-02 21:14:55 -0300

    Fix feature of creating review

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -2,9 +2,12 @@ class Api::V1::ReviewsController < ApplicationController
   before_action :authenticate, only: [:create]
   
   def create
+    place = Place.find(params[:place_id])
+
     review = Review.new
     review.rating = params[:rating]
     review.comment = params[:comment]
+    review.place = place
     review.save!
 
     render json: review.as_json(only: fields_only), status: :created
--- test/integration/create_review_test.rb
+++ test/integration/create_review_test.rb
@@ -2,18 +2,26 @@ require "test_helper"
 
 class CreateReviewTest < ActionDispatch::IntegrationTest
   test "create review" do
-    post api_v1_reviews_path, params: params, headers: generate_token
+    empire_state = places(:empire_state)
+
+    post api_v1_place_reviews_path(empire_state.id), 
+      params: params, headers: generate_token
 
     assert_response :created
 
     body = JSON.parse(response.body)
 
+    assert_not_nil body["id"]
     assert_equal 5, body["rating"]
     assert_equal "Awesome place", body["comment"]
+
+    assert_equal 1, empire_state.reviews.count
   end
 
   test "create review without token" do
-    post api_v1_reviews_path, params: params
+    empire_state = places(:empire_state)
+
+    post api_v1_place_reviews_path(empire_state.id), params: params
 
     assert_response :unauthorized
   end
